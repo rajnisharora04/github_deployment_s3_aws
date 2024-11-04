838e95e25ac2804c619ffc700434ca000070c1e2
Rajnish <64359789+rajnisharora04@users.noreply.github.com>
2024-11-04 15:00:20 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -3,7 +3,7 @@ terraform {
 
   backend "s3" {
     key    = "github-actions-cicd/terraform.tfstate" # the directory/file.tfstate
-    bucket = "ADD_YOUR_BUCKET_NAME_HERE"             # the bucket
-    region = "ADD_YOUR_REGION_NAME_HERE"             # the region
+    bucket = "rajnish-1234995"             # the bucket
+    region = "us-east-2"             # the region
   }
 }
